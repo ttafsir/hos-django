5ed4f6d6b841ed2b2b6774e946903a3bbd37c234
d3netxer <tommygun320@yahoo.com>
2014-05-14 22:13:49 -0400

    updated models with latest GeoDjango stuff

--- entries/models.py
+++ entries/models.py
@@ -1,28 +1,27 @@
 from django.db import models
 #extra stuff for GeoDjango
-from django.contrib.gis.db import models as geomodels
+from django.contrib.gis.db import models
 from django.contrib.gis.geos import *
-
-objects = geomodels.GeoManager()
 
 class ServiceProvider(models.Model):
 	provider =  models.AutoField(primary_key=True)
 	provider_name = models.CharField(max_length = 500)
 
 class Location(models.Model):
-    location_id = models.AutoField(primary_key=True,db_column='location_id')
-    #latitude and longitude need to be changed to string types for GeoDjango
-    latitude = models.CharField(max_length=15, blank=True, null=True)
-    longitude = models.CharField(max_length=15, blank=True, null=True)  
-    geom = geomodels.PointField(dim=3, geography=True, blank=True, null=True)
-    #address_information = models.CharField(max_length=300, blank=True)
-    
-def save(self):
-	if self.latitude != None and len(self.latitude) > 0:
-		lString = 'POINT(%s %s)' % (self.longitude.strip(), self.latitude.strip())
-		self.point = fromstr(lString)
-	self.last_modified = datetime.now()
-	super(Band, self).save()
+	location_id = models.AutoField(primary_key=True,db_column='location_id')
+	#latitude and longitude need to be changed to string types for GeoDjango
+	latitude = models.CharField(max_length=15, blank=True, null=True)
+	longitude = models.CharField(max_length=15, blank=True, null=True) 
+
+	#overriding the default manager with a GeoManager instance. 
+	geom = models.PointField(dim=3, geography=True, blank=True, null=True)
+	objects = models.GeoManager()
+
+	def save(self):
+		if self.latitude != None and len(self.latitude) > 0:
+			lString = 'POINT(%s %s)' % (self.longitude.strip(), self.latitude.strip())
+			self.point = fromstr(lString)
+		super(Location, self).save()
 	        
 class EffortInstance(models.Model):
 	TRAVELING_TEAM = 'TT'

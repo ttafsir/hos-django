1391fb81bc2cf20dba7def7a934e583a906e5ca9
d3netxer <tommygun320@yahoo.com>
2014-07-19 22:25:38 -0400

    modified deduplicate_database.py to check points inside admin areas

--- entries/deduplicate_database.py
+++ entries/deduplicate_database.py
@@ -11,11 +11,126 @@ sys.path.append(your_djangoproject_home)
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'hos2.settings'
  
-from entries.models import ServiceProvider,Location,EffortInstance,ServiceType,EffortInstanceServices, Spatial_cluster_results, Similar_strings
+from entries.models import ServiceProvider,Location,EffortInstance,ServiceType,EffortInstanceServices,Spatial_cluster_results,Admin_cluster_results,Similar_strings,haiti_adm3_minustah
  
 #insert code to clear Deduplication tables
 Similar_strings.objects.all().delete()
 Spatial_cluster_results.objects.all().delete()
+Admin_cluster_results.objects.all().delete()
+
+
+def SimilarityQuery (inputquery, querytype):
+
+	if querytype == 'admin_query':
+		Admin_cluster_results.objects.all().delete()
+		for d in inputquery:
+		
+			#write the temporary points in admin3 table
+			Admin_cluster_resultsObj = Admin_cluster_results()
+			Admin_cluster_resultsObj.date_start = EffortInstance.objects.get(location=d.location_id).date_start
+			Admin_cluster_resultsObj.date_end = EffortInstance.objects.get(location=d.location_id).date_end
+			Admin_cluster_resultsObj.effort_instance_id = EffortInstance.objects.get(location=d.location_id).effort_instance_id
+			Admin_cluster_resultsObj.latitude = d.latitude
+			Admin_cluster_resultsObj.longitude = d.longitude
+			Admin_cluster_resultsObj.location_id = d.location_id
+			provider_num = EffortInstance.objects.get(location=d.location_id).service_provider
+			Admin_cluster_resultsObj.service_provider = EffortInstance.objects.get(location=d.location_id).service_provider
+			Admin_cluster_resultsObj.provider_name = ServiceProvider.objects.get(provider=provider_num.provider).provider_name
+			Admin_cluster_resultsObj.nearby_points = qs.count()
+			Admin_cluster_resultsObj.point_checked = False
+			Admin_cluster_resultsObj.save()
+		
+	for a in inputquery:
+		print("a point: ")
+		print(a.point)
+
+		pnt2 = a.point
+	
+		if querytype == 'proximity_query':
+		
+			qs2 = Spatial_cluster_results.objects.filter(point__dwithin=(pnt2,D(m=10)))
+			
+		if querytype == 'admin_query':
+		
+			print('must be admin_query')
+			qs2 = inputquery
+			
+	
+		print('a location id: ')
+		print(a.location_id)
+	
+		if querytype == 'proximity_query':
+		
+			print('printing point_checked: ')
+			print(Spatial_cluster_results.objects.get(location=a.location_id).point_checked)
+	
+			#Mark the point that is checking it's neighbors True, so it's neighbors won't recheck it
+			BoolMod = Spatial_cluster_results.objects.get(location=a.location_id)
+			BoolMod.point_checked = True
+			BoolMod.save()
+	
+			print('printing point_checked 2nd try: ')
+			print(Spatial_cluster_results.objects.get(location=a.location_id).point_checked)
+			
+		if querytype == 'admin_query':
+		
+			#Mark the point that is checking it's neighbors True, so it's neighbors won't recheck it
+			BoolMod = Admin_cluster_results.objects.get(location=a.location_id)
+			BoolMod.point_checked = True
+			BoolMod.save()
+	
+			#print(qs2)
+	
+		for b in qs2:
+			print("b point: ")
+			print(b.point)
+	
+			if querytype == 'proximity_query':
+				if Spatial_cluster_results.objects.get(location=b.location_id).point_checked == False:
+	
+					#compare similarities, need to add similarities to similar_strings table
+					a_point_name = ServiceProvider.objects.get(provider=EffortInstance.objects.get(location=a.location_id).service_provider.provider).provider_name
+					b_point_name = ServiceProvider.objects.get(provider=EffortInstance.objects.get(location=b.location_id).service_provider.provider).provider_name
+				
+					print(a_point_name)
+					print(b_point_name)
+
+					print("difflib ratio: ")
+					print(difflib.SequenceMatcher(None, a_point_name, b_point_name).ratio())
+
+					Similar_stringsObj = Similar_strings()
+
+					if querytype == 'proximity_query':
+						Similar_stringsObj.effort_instance_id = Spatial_cluster_results.objects.get(location=a.location_id)
+
+					Similar_stringsObj.related_string_id = EffortInstance.objects.get(location=b.location_id).effort_instance_id
+					Similar_stringsObj.similarity_score = difflib.SequenceMatcher(None, a_point_name, b_point_name).ratio()
+
+					Similar_stringsObj.save()
+			
+			if querytype == 'admin_query':
+				if Admin_cluster_results.objects.get(location=b.location_id).point_checked == False:
+	
+					#compare similarities, need to add similarities to similar_strings table
+					a_point_name = ServiceProvider.objects.get(provider=EffortInstance.objects.get(location=a.location_id).service_provider.provider).provider_name
+					b_point_name = ServiceProvider.objects.get(provider=EffortInstance.objects.get(location=b.location_id).service_provider.provider).provider_name
+					
+					print(a_point_name)
+					print(b_point_name)
+
+					print("difflib ratio: ")
+					print(difflib.SequenceMatcher(None, a_point_name, b_point_name).ratio())
+
+					Similar_stringsObj = Similar_strings()
+
+					if querytype == 'proximity_query':
+						Similar_stringsObj.effort_instance_id = Spatial_cluster_results.objects.get(location=a.location_id)
+
+					Similar_stringsObj.related_string_id = EffortInstance.objects.get(location=b.location_id).effort_instance_id
+					Similar_stringsObj.similarity_score = difflib.SequenceMatcher(None, a_point_name, b_point_name).ratio()
+
+					Similar_stringsObj.save()
+s
 
 queryset = Location.objects.all()
 
@@ -30,14 +145,16 @@ total_point_count = 0
 
 total_point_sum = 0
 
+#loops through each point that has a geom
 for p in queryset:
 	#print(p.point)
 	pnt = p.point
 	
-	qs = Location.objects.filter(point__dwithin=(pnt,D(m=10)))
+	#doesn't work when I changed the points to geometries from geography types
+	#qs = Location.objects.filter(point__dwithin=(pnt,D(m=10)))
 	
 	#alternative query that does the same thing
-	#qs = Location.objects.filter(point__distance_lt=(pnt,D(m=10)))
+	qs = Location.objects.filter(point__distance_lt=(pnt,D(m=10)))
 	
 	#print(qs.count())
 	
@@ -82,53 +199,44 @@ print(total_point_sum)
 
 queryset2 = Spatial_cluster_results.objects.all()
 
-for a in queryset2:
-	print("a point: ")
-	print(a.point)
 
-	pnt2 = a.point
-	
-	qs2 = Spatial_cluster_results.objects.filter(point__dwithin=(pnt2,D(m=10)))
-	
-	print('a location id: ')
-	print(a.location_id)
-	
-	print('printing point_checked: ')
-	print(Spatial_cluster_results.objects.get(location=a.location_id).point_checked)
-	
-	BoolMod = Spatial_cluster_results.objects.get(location=a.location_id)
-	BoolMod.point_checked = True
-	BoolMod.save()
-	
-	print('printing point_checked 2nd try: ')
-	print(Spatial_cluster_results.objects.get(location=a.location_id).point_checked)
-	
-	#print(qs2)
-	
-	for b in qs2:
-		print("b point: ")
-		print(b.point)
-		
-		if Spatial_cluster_results.objects.get(location=b.location_id).point_checked == False:
-		
-			#compare similarities, need to add similarities to similar_strings table
-			a_point_name = ServiceProvider.objects.get(provider=EffortInstance.objects.get(location=a.location_id).service_provider.provider).provider_name
-			b_point_name = ServiceProvider.objects.get(provider=EffortInstance.objects.get(location=b.location_id).service_provider.provider).provider_name
-			print(a_point_name)
-			print(b_point_name)
-		
-			print("difflib ratio: ")
-			print(difflib.SequenceMatcher(None, a_point_name, b_point_name).ratio())
-		
-			Similar_stringsObj = Similar_strings()
-		
-			#Similar_stringsObj.effort_instance_id = Deduplication_results.objects.get(location=a.location_id)
-			Similar_stringsObj.effort_instance_id = Spatial_cluster_results.objects.get(location=a.location_id)
-		
-			Similar_stringsObj.related_string_id = EffortInstance.objects.get(location=b.location_id).effort_instance_id
-			Similar_stringsObj.similarity_score = difflib.SequenceMatcher(None, a_point_name, b_point_name).ratio()
 
-			Similar_stringsObj.save()
+#finding all of the admin3 boundaries
+EffortInstance_queryset = EffortInstance.objects.all()
+
+mylist = []
+
+for b in EffortInstance_queryset:
+
+	if b.adm_3:
+		print(b.adm_3)
+		mylist.append(b.adm_3)
+		
+myset = set(mylist)
+
+print('printing output: ')
+print myset
+
+#need to compare to all points within the admin 3 polygon
+#looping through all of the unique admin3 polygons
+for c in myset:
+	print(c)
+
+	poly = c.geom
+	
+	#spatial query finds all points within each individual polygon
+	point_qs = Location.objects.filter(point__within=poly)
+	
+	print(point_qs)
+	#print(point_qs)
+	print(len(point_qs))
+	
+	if len(point_qs) > 1:
+	
+		SimilarityQuery(point_qs, 'admin_query')
+		
 			
+
+SimilarityQuery(queryset2, 'proximity_query')
 			
 			
--- entries/models.py
+++ entries/models.py
@@ -14,7 +14,8 @@ class Location(models.Model):
 	longitude = models.CharField(max_length=15, blank=True, null=True) 
 
 	#overriding the default manager with a GeoManager instance. 
-	point = models.PointField(dim=3, geography=True, blank=True, null=True)
+	#Changing geography to equal False, and dim(dimension to 2) This will use a geom type and be compatible with the admin polygons
+	point = models.PointField(dim=2, geography=False, blank=True, null=True)
 	objects = models.GeoManager()
 
 	def save(self):
@@ -201,6 +202,47 @@ class Spatial_cluster_results(models.Model):
 			lString = 'POINT(%s %s)' % (self.longitude.strip(), self.latitude.strip())
 			self.point = fromstr(lString)
 		super(Spatial_cluster_results, self).save()
+		
+#table to store the admin3 point in polygon processing
+class Admin_cluster_results(models.Model):
+	TRAVELING_TEAM = 'TT'
+	CLINIC = 'CL'
+	HOSPITAL = 'HL'
+	PROVIDER_TYPE_OPTIONS = (
+		(TRAVELING_TEAM, 'Traveling Team'),
+		(CLINIC, 'Clinic'),
+		(HOSPITAL, 'Hospital'),
+	)
+	
+	effort_instance_id = models.IntegerField(primary_key=False, blank=True, null=True)
+	service_provider = models.ForeignKey(ServiceProvider, blank=True, null=True)
+	location = models.ForeignKey(Location, blank=True, null=True)
+	adm_3 = models.ForeignKey(haiti_adm3_minustah, blank=True, null=True)
+	provider_type = models.CharField(max_length=2, choices=PROVIDER_TYPE_OPTIONS, default=CLINIC)
+	date_start = models.DateTimeField(auto_now=False, null=True)
+	date_end = models.DateTimeField(auto_now=False, null=True)
+	
+	provider_name = models.CharField(max_length = 500)
+	
+	nearby_points = models.IntegerField(primary_key=False, blank=True, null=True)
+	
+	point_checked = models.NullBooleanField(primary_key=False, blank=True)
+	
+	#location_id = models.AutoField(primary_key=True,db_column='location_id')
+	#latitude and longitude need to be changed to string types for GeoDjango
+	latitude = models.CharField(max_length=15, blank=True, null=True)
+	longitude = models.CharField(max_length=15, blank=True, null=True) 
+
+	#overriding the default manager with a GeoManager instance. 
+	point = models.PointField(dim=3, geography=True, blank=True, null=True)
+	objects = models.GeoManager()
+	
+	
+	def save(self):
+		if self.latitude != None and len(self.latitude) > 0:
+			lString = 'POINT(%s %s)' % (self.longitude.strip(), self.latitude.strip())
+			self.point = fromstr(lString)
+		super(Admin_cluster_results, self).save()
 		
 class Similar_strings(models.Model):
 	Similar_strings_id = models.AutoField(primary_key=True)
